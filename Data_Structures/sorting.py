e92c44f3018b608151ffece4623baa0e5b5b1d84
Pranay Tuladhar <pranaytuladhar101@gmail.com>
2025-03-11 18:53:59 +0545

    fixed the code for Bubble sort

--- Data_Structures/sorting.py
+++ Data_Structures/sorting.py
@@ -31,8 +31,12 @@ def makeArrayBox(index,element):
 
 class BubbleSort(Scene):
     def construct(self):
-        elements = [2,5,3,6,8,9,1]
-        indexes = [0,1,2,3,4,5,6]
+        text = Text("BubbleSort")
+        self.play(Write(text),run_time=3)
+        self.play(text.animate.shift(UP*3))
+
+        elements = [2,5,3,6,8,9,1,0,-4]
+        indexes = [0,1,2,3,4,5,6,7,8]
         allElements = VGroup()
         for ele,idx in zip(elements,indexes):
             temp = makeArrayBox(idx,ele)
@@ -40,11 +44,17 @@ class BubbleSort(Scene):
 
         allElements.arrange(RIGHT,aligned_edge = LEFT,buff=0.5)
         self.play(FadeIn(allElements))
-
-        for i in range(0,7):
-            for j in range(0,6):
+        arrow1 = Arrow(start=UP,end=DOWN)
+        arrow2 = Arrow(start=UP,end=DOWN)
+        arrow1.next_to(allElements[0],UP,buff=0.1)
+        arrow2.next_to(allElements[0],UP,buff=0.1)
+        for i in range(0,9):
+            for j in range(0,8):
+                self.play(arrow1.animate.next_to(allElements[j],UP,buff=0.1),arrow2.animate.next_to(allElements[j+1],UP,buff=0.1))
                 if allElements[j][2].get_value() > allElements[j+1][2].get_value():
-                    temp = allElements[j+1][2].get_value()
-                    allElements[j+1][2].set_value(allElements[j][2].get_value())
-                    allElements[j][2].set_value(temp)
-        self.interactive_embed()
+                    self.play(Swap(allElements[j][2],allElements[j+1][2]))
+                    temp = allElements[j][2]
+                    allElements[j][2] = allElements[j+1][2]
+                    allElements[j+1][2] = temp
+
+        #self.interactive_embed()
